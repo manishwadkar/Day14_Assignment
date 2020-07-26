cdf8207e7d361ce64781d84c021c7aa192485c5c
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-26 17:33:37 +0530

    UC1-validFirstName implemented

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "Welcome to User Registration system"
+
+echo "Valid username should start with capital letter and minimum three letters"
+read -r -p "Enter first name: " fname
+pattern_fname="^[ABCDEFGHIJKLMNOPQRSTUVWXYZ]{1}[a-z]{2,}$"
+
+if [[ $fname =~ $pattern_fname ]]
+then
+	echo "First name entered is valid"
+else
+	echo "Invalid first name"
+fi
