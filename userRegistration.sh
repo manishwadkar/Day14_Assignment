903d15d8ebc42818c09f22df761df9f308be946d
“manishwadkar” <wadkarmanish@gmail.com>
2020-07-26 17:50:27 +0530

    UC2-validLastName implemented

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,7 +2,7 @@
 
 echo "Welcome to User Registration system"
 
-echo "Valid username should start with capital letter and minimum three letters"
+echo "Valid firstname should start with capital letter and minimum three letters"
 read -r -p "Enter first name: " fname
 pattern_fname="^[ABCDEFGHIJKLMNOPQRSTUVWXYZ]{1}[a-z]{2,}$"
 
@@ -11,4 +11,19 @@ then
 	echo "First name entered is valid"
 else
 	echo "Invalid first name"
+	echo "Please being the process of User Registration from start"
+	exit
+fi
+
+echo "Valid lastname should start with capital letter and minimum three letters"
+read -r -p "Enter last name: " lname
+pattern_lname="^[ABCDEFGHIJKLMNOPQRSTUVWXYZ]{1}[a-z]{2,}$"
+
+if [[ $lname =~ $pattern_lname ]]
+then
+        echo "Last name entered is valid"
+else
+        echo "Invalid last name"
+	echo "Please being the process of User Registration from start"
+	exit
 fi
